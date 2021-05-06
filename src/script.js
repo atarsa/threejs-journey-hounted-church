27eb8d2812a02de16bc98e3d89901193b3612b64
AT <agnieszkatarsa@gmail.com>
2021-05-06 19:34:46 +0100

    Add shadows

--- src/script.js
+++ src/script.js
@@ -65,7 +65,7 @@ grassNormalTexture.wrapT = THREE.RepeatWrapping;
 grassAmbientOclusionTexture.wrapT = THREE.RepeatWrapping;
 grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
 
-grassColorTexture.wrapS  = THREE.RepeatWrapping;
+grassColorTexture.wrapS = THREE.RepeatWrapping;
 grassNormalTexture.wrapS = THREE.RepeatWrapping;
 grassAmbientOclusionTexture.wrapS = THREE.RepeatWrapping;
 grassRoughnessTexture.wrapS = THREE.RepeatWrapping;
@@ -238,6 +238,7 @@ for (let i = 0; i < 50; i++) {
   grave.position.set(x, y, z);
   grave.rotation.y = (Math.random() - 0.5) * 0.4;
   grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  grave.castShadow = true;
   graves.add(grave);
 }
 
@@ -283,6 +284,18 @@ scene.add(moonLight);
 const doorLight = new THREE.PointLight('#ff7d46', 1, 7);
 doorLight.position.set(0, 3, 2.2);
 church.add(doorLight);
+
+/**
+ * Ghosts
+ */
+const ghost1 = new THREE.PointLight('#ff00ff', 2, 3);
+scene.add(ghost1);
+
+const ghost2 = new THREE.PointLight('#ffff00', 2, 3);
+scene.add(ghost2);
+
+const ghost3 = new THREE.PointLight('#00ffff', 2, 3);
+scene.add(ghost3);
 /**
  * Sizes
  */
@@ -335,6 +348,48 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor('#262837');
 
 /**
+ * Shadows
+ */
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+moonLight.castShadow = true;
+doorLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+
+walls1.castShadow = true;
+walls2.castShadow = true;
+
+bush1.castShadow = true;
+bush2.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
+
+floor.receiveShadow = true;
+
+moonLight.shadow.mapSize.width = 256;
+moonLight.shadow.mapSize.height = 256;
+moonLight.shadow.camera.far = 15;
+
+doorLight.shadow.mapSize.width = 256;
+doorLight.shadow.mapSize.height = 256;
+doorLight.shadow.camera.far = 7;
+
+ghost1.shadow.mapSize.width = 256;
+ghost1.shadow.mapSize.height = 256;
+ghost1.shadow.camera.far = 7;
+
+ghost2.shadow.mapSize.width = 256;
+ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.camera.far = 7;
+
+ghost3.shadow.mapSize.width = 256;
+ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.camera.far = 7;
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
@@ -342,6 +397,23 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
+  // update ghosts
+  const ghost1Angle = elapsedTime * 0.5;
+  ghost1.position.x = Math.cos(ghost1Angle) * 7;
+  ghost1.position.y = Math.sin(ghost1Angle) * 2;
+  ghost1.position.z = Math.sin(ghost1Angle) * 7;
+
+  const ghost2Angle = -elapsedTime * 0.32;
+  ghost2.position.x = Math.cos(ghost2Angle) * 7;
+  ghost2.position.y = Math.sin(ghost2Angle) * 3 + Math.sin(ghost2Angle * 2.5);
+  ghost2.position.z = Math.sin(ghost2Angle) * 7;
+
+  const ghost3Angle = elapsedTime * 0.18;
+  ghost3.position.x =
+    Math.cos(ghost3Angle) * (10 + Math.sin(elapsedTime * 0.32));
+  ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2);
+  ghost3.position.z =
+    Math.sin(ghost3Angle) * (10 + Math.sin(elapsedTime * 0.52));
   // Update controls
   controls.update();
 
